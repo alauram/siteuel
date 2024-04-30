6c2d109f79fd97590db9f10b5a29af8fe7e49988
alauram <128840291+alauram@users.noreply.github.com>
2024-04-30 20:24:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ document.getElementById('excelButton').addEventListener('click', function () {
 });
 
 document.getElementById('docsButton').addEventListener('click', function () {
-    window.location.href = 'siteuel/documentos/index.html';
+    window.location.href = '/siteuel/documentos/index.html';
 });
 
 document.getElementById('apresentacoesButton').addEventListener('click', function () {
